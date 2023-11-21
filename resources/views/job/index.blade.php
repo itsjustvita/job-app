aa7dede5380e94978f140e401d2e280e874a0da9
moonflag-vita <vitalis@moonflag.de>
2023-11-21 07:43:01 +0100

    Breadcrumbs Component, other minor adjustments

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,4 +1,13 @@
 <x-layout>
+    <x-breadcrumbs :links="['Jobs' => route('jobs.index') ]" class="mb-4" />
+    <x-card class="mb-4 text-sm">
+        <div class="mb-4 grid grid-cols-2 gap-4">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+        </div>
+    </x-card>
     @foreach($jobs as $job)
         <x-job-card class="mb-4" :$job>
             <div>
--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <x-job-card :$job >
+    <x-breadcrumbs :links="['Jobs' => route('jobs.index'), $job->title => '#' ]" class="mb-4" />
 
-    </x-job-card>
+    <x-job-card :$job />
 </x-layout>
