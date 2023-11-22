1f0c392032d02bab9e84a0d965cbe1269a4c7902
moonflag-vita <vitalis@moonflag.de>
2023-11-22 10:20:45 +0100

    TextInput Component, Removed Description from List

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -2,8 +2,18 @@
     <x-breadcrumbs :links="['Jobs' => route('jobs.index') ]" class="mb-4" />
     <x-card class="mb-4 text-sm">
         <div class="mb-4 grid grid-cols-2 gap-4">
-            <div>1</div>
-            <div>2</div>
+            <div>
+                <div class="mb-1 font-semibold">Suche</div>
+                <x-text-input placeholder="Suche" name="search" value="" />
+            </div>
+            <div>
+                <div class="mb-1 font-semibold">Gehalt</div>
+                <div class="flex space-x-2">
+                    <x-text-input placeholder="Von" name="min_salary" value="" />
+                    <x-text-input placeholder="Bis" name="max_salary" value="" />
+
+                </div>
+            </div>
             <div>3</div>
             <div>4</div>
         </div>
